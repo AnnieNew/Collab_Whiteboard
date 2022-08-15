a8a5019300afcfbd7fbe95445c7cb31a348cc181
charmanderli <lsfchar@gmail.com>
2022-08-15 03:36:22 -0700

    fixed cors

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,18 @@
-var app = require("express")();
-let http = require("http").createServer(app);
-let io = require("socket.io")(http);
+// var app = require("express")();
+// let http = require("http").createServer(app);
+// let io = require("socket.io")(http);
+const express = require("express");
+const socketIo = require("socket.io");
+const http = require("http");
+const PORT = process.env.PORT || 5000;
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
 io.on("connection", function (socket) {
   console.log("a user connected");
   socket.on("canvas-data", (data) => {
@@ -13,7 +25,10 @@ io.on("connection", function (socket) {
   });
 });
 
-var server_port = 5500;
-http.listen(server_port, function () {
-  console.log("listening on: " + server_port);
+server.listen(PORT, function () {
+  console.log("listening on: " + PORT);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("hello world");
 });
